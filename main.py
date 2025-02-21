9ee9602359bbe274e12808cc8dc55ff2438576d4
ktg753 <154485530+ktg753@users.noreply.github.com>
2025-02-21 12:47:02 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if support_type == "CAT Tool":
                          "레지스트리 편집기 실행: Windows 키 + R > regedit 입력 후 확인<br>"
                          "레지스트리 경로: 컴퓨터\\HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Trados\\Studio17License<br>"
                          "경로 내 'CheckedOutEdition' 데이터 값을 'WorkgroupEdition'으로 수정해 주세요.<br><br> 해결되지 않을 경우 아래 링크를 통해 지원 요청 부탁드립니다.", unsafe_allow_html=True)
-                st.markdown("[지원 요청하기.](https://globalway.sharepoint.com/_layouts/15/listform.aspx?PageType=8&ListId=%7B5B0294C8-DF48-469F-92E1-3311A6F850EA%7D&RootFolder=%2FLists%2FSupport&Source=https%3A%2F%2Fglobalway.sharepoint.com%2FLists%2FSupport%2FAllItems.aspx%3FsortField%3D%255Fxc644%255F%255Fxb8cc%255F%255Fx0020%255F%255Fxc2dc%255F%255Fxac%26isAscending%3Dfalse%26groupBy%3D%255Fxc885%255F%255Fxb958%255F%26viewid%3D3a277bcd%252De09e%252D43f9%252Dbfe8%252D7ae30421cc89%26view%3D7%26q%3D%25EB%259D%25BC%25EC%259D%25B4%25EC%2584%25A0%25EC%258A%25A4&ContentTypeId=0x010080F3A6FBB7256143AA68746BF8C8EDF5)")
+                st.markdown("[지원 요청하기](https://globalway.sharepoint.com/_layouts/15/listform.aspx?PageType=8&ListId=%7B5B0294C8-DF48-469F-92E1-3311A6F850EA%7D&RootFolder=%2FLists%2FSupport&Source=https%3A%2F%2Fglobalway.sharepoint.com%2FLists%2FSupport%2FAllItems.aspx%3FsortField%3D%255Fxc644%255F%255Fxb8cc%255F%255Fx0020%255F%255Fxc2dc%255F%255Fxac%26isAscending%3Dfalse%26groupBy%3D%255Fxc885%255F%255Fxb958%255F%26viewid%3D3a277bcd%252De09e%252D43f9%252Dbfe8%252D7ae30421cc89%26view%3D7%26q%3D%25EB%259D%25BC%25EC%259D%25B4%25EC%2584%25A0%25EC%258A%25A4&ContentTypeId=0x010080F3A6FBB7256143AA68746BF8C8EDF5)")
 
         if trados_issue == "프로젝트 생성/설정":
             project_issue = st.radio(
@@ -49,6 +49,6 @@ if support_type == "서버/네트워크/Windows":
     cat_issue = st.radio(
         "서버/네트워크/Windows 이슈",
         ["인터넷 속도 느림", "네트워크 드라이브 이슈", "Windows 이슈"], index=None)
-   
+
 
     st.success(f"선택한 이슈: {cat_issue}")
